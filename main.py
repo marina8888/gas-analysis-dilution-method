0e0d42c354c96aec8086e63cc1362f57a97a5fc5
Marina Kovaleva <38610356+marina8888@users.noreply.github.com>
2020-04-21 23:39:41 +0900

    added all libraries

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import openpyxl as xl
+
+
+Mode=[]
+Dim increm As Integer 'multiplier to select smallxvalue for the correct gas
+Dim r As Integer
+Dim coliterator As Integer
+Dim GasCurrentSelector() As Long 'Dilution gas type and condition selector
+Dim TotalRows As Integer
+Dim Counter As Long 'Your counting variable to step through your entire array
+Dim Result As Boolean 'Your result variable, will be set to False if something is not equal to 0, otherwise it's default value is True
+Dim InletGasNum As Integer 'Constant, number of inlet gases including dilution gas (still counted as +1 when 2 difference concentrations are used)
+Dim TodaysDate As Integer 'Used to map datalogger vaues to correct spreadsheet- column 3 should match logger spreadshet
+Dim SearchValue, MatchRange As Range
+Dim wsnum As Integer 'Worksheet number for datalogger
+Dim WorksheetName As Variant
+Dim MyArr As Variant
+
+
+with open("some.xls", "wb") as excel_file:
+    #Do something
+pass
+
+
 
 

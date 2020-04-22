36eb86bacca563491e7ae4d32827b7116683f9f9
Marina Kovaleva <38610356+marina8888@users.noreply.github.com>
2020-04-23 04:03:52 +0900

    split by mode added

--- src/classes.py
+++ src/classes.py
@@ -10,26 +10,32 @@ class Workbook():
         self.workbook_name = workbook_name
         self.start_row_number = start_row_number
         self.df = self.prepare_df()
+        print(self.df.columns)
         self.df=self.split_df()
-        # self.df = self.df.values.tolist()
-        #
+        # #
         # # defining all variables that are stored directly in spreadsheet
         # self.gas_type = self.lists_to_array(int, 7)
         # print("gases types are: " + str(self.gas_type))
 
     def prepare_df(self):
-        self.df = self.df.iloc[self.start_row_number - 2:]
+        #cut sheet for relevant data only
+        self.df = self.df.iloc[self.start_row_number - 3:]
         self.df = self.df.reset_index(drop=True)
-        self.df = self.df.T
+        #rename columns
+        new_header = self.df.iloc[0]  # grab the first row for the header
+        self.df = self.df[1:]  # take the data less the header row
+        self.df.columns = new_header  # set the header row as the df header
         return self.df
 
     def split_df(self):
-        self.df= self.df[self.df['Unnamed: 7'] == '0']
+        self.df = self.df[self.df['Tracer gas type'] == '0']
         print(self.df)
         return self.df
 
 
     def lists_to_array(self, datatype, col_num: int):
+        self.df = self.df.T
+        self.df = self.df.values.tolist()
         list = (self.df[col_num])
         for i in range(0, len(list)):
             list[i] = datatype(list[i])

89b4edb64a7a1d691925f71a8ef4bf811c873d9f
Marina Kovaleva <marinakovaleva0@gmail.com>
2020-04-23 21:56:43 +0900

    split by mode added

--- src/classes.py
+++ src/classes.py
@@ -9,29 +9,48 @@ class Workbook():
         self.df = pd.read_excel(workbook_name, sheet_name='Main')
         self.workbook_name = workbook_name
         self.start_row_number = start_row_number
+
         self.df = self.prepare_df()
-        print(self.df.columns)
-        self.df=self.split_df()
-        # #
+        self.df_mode0 = self.split_df_mode0()
+        self.df_mode1 =self.split_df_mode1()
+        self.df_mode2 = self.split_df_mode2()
+
+
         # # defining all variables that are stored directly in spreadsheet
         # self.gas_type = self.lists_to_array(int, 7)
         # print("gases types are: " + str(self.gas_type))
 
     def prepare_df(self):
-        #cut sheet for relevant data only
+        # cut sheet for relevant data only
         self.df = self.df.iloc[self.start_row_number - 3:]
         self.df = self.df.reset_index(drop=True)
-        #rename columns
+
+        # rename columns
         new_header = self.df.iloc[0]  # grab the first row for the header
         self.df = self.df[1:]  # take the data less the header row
         self.df.columns = new_header  # set the header row as the df header
         return self.df
 
-    def split_df(self):
-        self.df = self.df[self.df['Tracer gas type'] == '0']
-        print(self.df)
-        return self.df
+    def split_df_mode0(self):
+        #column 7 is called Tracer gas type - this works
+        self.df_mode0 = self.df[self.df['Tracer gas type'] == '0']
+        print(self.df_mode0.iloc[:,7])
+        return self.df_mode0
 
+
+    def split_df_mode1(self):
+        # I need something like this, where for this file the print statement would return 1,1,1,1...:
+        for row_index in self.df[7]:
+            self.df_mode1 = self.df[self.df['Tracer gas type', row_index] == ['Tracer gas type', row_index+1] ]
+        print(self.df_mode2.iloc[:,7])
+        return self.df_mode1
+
+    def split_df_mode2(self):
+        #I need something like this, where this print statement returns 1,2,1,2,1,2,1,2...:
+        for row_index in self.df[7]:
+            self.df_mode2 = self.df[self.df['Tracer gas type', row_index] =='1' & ['Tracer gas type', row_index+1] =='2']]
+        print(self.df_mode2.iloc[:,7])
+        return self.df_mode2
 
     def lists_to_array(self, datatype, col_num: int):
         self.df = self.df.T

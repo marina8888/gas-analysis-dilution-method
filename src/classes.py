d8484ae6f0e0ddd98603d47158428c9d7bcb4eed
Marina Kovaleva <38610356+marina8888@users.noreply.github.com>
2020-04-26 19:26:48 +0900

    column structure added and new cols init

--- src/classes.py
+++ src/classes.py
@@ -10,14 +10,14 @@ class Workbook():
         self.workbook_name = workbook_name
         self.start_row_number = start_row_number
 
-        self.df = self.prepare_df
-        self.df_mode0 = self.split_df_mode0()
-        self.df_mode0 = self.upper_eq()
-        # # defining all variables that are stored directly in spreadsheet
-        # self.gas_type = self.lists_to_array(int, 7)
-        # print("gases types are: " + str(self.gas_type))
+        self.prepare_df()
+        self.init_cols()
 
-    @property
+        # called in this order because mode 1 is least selective
+        self.df_mode0 = self.split_df_mode0()
+        self.df_mode1 = self.split_df_mode1()
+        self.df_mode2 = self.split_df_mode2()
+
     def prepare_df(self):
         # cut sheet for relevant data only
         self.df = self.df.iloc[self.start_row_number - 3:]
@@ -27,27 +27,78 @@ class Workbook():
         new_header = self.df.iloc[0]  # grab the first row for the header
         self.df = self.df[1:]  # take the data less the header row
         self.df.columns = new_header  # set the header row as the df header
-        self.df['upper_eq']= None
-        return self.df
 
+    # initialising new columns for storing variables
+    def init_cols(self):
+        self.df['Qd1_upper'] = None
+        self.df['Qd1_lower'] = None
+        self.df['Qd2_upper'] = None
+        self.df['Qd2_lower'] = None
+        self.df['Epsilontr1'] = None
+        self.df['Epsilontr2'] = None
+        self.df['Qs'] = None
+        self.df['Z'] = None
+        self.df['upper_eq'] = None
+        self.df['lower_eq'] = None
+
+        #values per gas
+        self.df['x1']= None
+        self.df['x2']= None
+        self.df['X1'] = None
+        self.df['X2'] = None
+        self.df['X_Xi1'] = None
+        self.df['X_Xi2'] = None
+        self.df['X_Epsilon1'] = None
+        self.df['X_Epsilon2'] = None
+        self.df['X_Q1'] = None
+        self.df['X_Q2'] = None
+        self.df['X_x']=None
+        self.df['X_x'] = None
+
+
+    # splitting df by tracer gas column into mode0 (no gas) mode 1 and 2
     def split_df_mode0(self):
-        # column 7 is called Tracer gas type - this works
         self.df_mode0 = self.df[self.df['Tracer gas type'] == '0']
         return self.df_mode0
 
-    # def split_df_mode1(self):
-    #     # I need something like this, where for this file the print statement would return 1,1,1,1...:
-    #     for row_index in self.df[7]:
-    #         self.df_mode1 = self.df[self.df['Tracer gas type', row_index] == ['Tracer gas type', row_index+1] ]
-    #     print(self.df_mode2.iloc[:,7])
-    #     return self.df_mode1
+    def split_df_mode1(self):
+        # create a new empty dataframe with same columns and number of columns as previous dataframe
+        df_mode1 = pd.DataFrame().reindex_like(self.df)
+        df_mode1 = df_mode1.iloc[0:0]
+        # initalising first and second col values to check
+        last_tgt = ''
+        this_tgt = ''
 
-    # def split_df_mode2(self):
-    #     #I need something like this, where this print statement returns 1,2,1,2,1,2,1,2...:
-    #     for row_index in self.df[7]:
-    #         self.df_mode2 = self.df[self.df['Tracer gas type', row_index] =='1' & ['Tracer gas type', row_index+1] =='2']]
-    #     print(self.df_mode2.iloc[:,7])
-    #     return self.df_mode2
+        # go over all rows in df.values
+        for row_id in range(0, len(self.df.values)):
+            this_tgt = self.df.iloc[row_id][7]
+            # leave out the first row for comparison
+            if (last_tgt != ''):
+                # if dataframe rows are consecutively equal, write to new dataframe
+                if ((this_tgt == '1' and last_tgt == '1') or (this_tgt == '2' and last_tgt == '2')):
+                    df_mode1.loc[len(df_mode1)] = self.df.iloc[row_id - 1]
+            last_tgt = this_tgt
+        return df_mode1
+
+    def split_df_mode2(self):
+        # create a new empty dataframe with same columns and number of columns as previous dataframe
+        df_mode2 = pd.DataFrame().reindex_like(self.df)
+        df_mode2 = df_mode2.iloc[0:0]
+        # initalising first and second col values to check
+        last_tgt = ''
+        this_tgt = ''
+
+        # go over all rows in df.values
+        for row_id in range(0, len(self.df.values)):
+            this_tgt = self.df.iloc[row_id][7]
+            # leave out the first row for comparison
+            if (last_tgt != ''):
+                # if the last tgt was 1 and this is 2 then write both rows to the new dataset
+                if (this_tgt == '2' and last_tgt == '1'):
+                    df_mode2.loc[len(df_mode2)] = self.df.iloc[row_id - 1]
+                    df_mode2.loc[len(df_mode2)] = self.df.iloc[row_id]
+            last_tgt = this_tgt
+        return df_mode2
 
     def lists_to_array(self, datatype, col_num: int):
         self.df = self.df.T
@@ -57,11 +108,72 @@ class Workbook():
             list[i] = datatype(list[i])
         return list
 
-    def upper_eq(self):
-        print((4.76 / (self.df_mode0.iloc[:, 32])) * (2 * self.df_mode0.iloc[:, 33]) + (
-                    0.75 * self.df_mode0.iloc[:, 35]))
+    def concat(self):
+        split_dataframes = [self.df_mode0, self.df_mode1, self.df_mode2]
+        self.df = pd.concat(split_dataframes)
+        self.df = self.df.reset_index(drop=True)
 
-        self.df_mode0.loc[0:len(self.df.index),'upper_eq']= (4.76 / (self.df_mode0.iloc[:, 32])) * (2 * self.df_mode0.iloc[:, 33]) + (
-                    0.75 * self.df_mode0.iloc[:, 35])
-        print(self.df_mode0.iloc[:, -1])
-        return self.df_mode0
+
+    # below are methods for calculating values in all three dataframes
+    def upper_eq(self):
+        if self.df_mode0 is not None:
+            self.df_mode0['upper_eq'] = (4.76 / (self.df_mode0.iloc[:, 32])) * ((2 * self.df_mode0.iloc[:, 33])
+                                                                                + (0.75 * self.df_mode0.iloc[:, 35]))
+        if self.df_mode1 is not None:
+            self.df_mode1['upper_eq'] = (4.76 / (self.df_mode1.iloc[:, 32])) * ((2 * self.df_mode1.iloc[:, 33])
+                                                                                + (0.75 * self.df_mode1.iloc[:, 35]))
+        if self.df_mode2 is not None:
+            self.df_mode2['upper_eq'] = (4.76 / (self.df_mode2.iloc[:, 32])) * ((2 * self.df_mode2.iloc[:, 33])
+                                                                                + (0.75 * self.df_mode2.iloc[:, 35]))
+
+    def lower_eq(self):
+        if self.df_mode0 is not None:
+            self.df_mode0['lower_eq'] = (4.76 / (self.df_mode0.iloc[:, 31])) * ((2 * self.df_mode0.iloc[:, 34])
+                                                                                + (0.75 * self.df_mode0.iloc[:, 36]))
+        if self.df_mode1 is not None:
+            self.df_mode1['lower_eq'] = (4.76 / (self.df_mode1.iloc[:, 31])) * ((2 * self.df_mode1.iloc[:, 34])
+                                                                                + (0.75 * self.df_mode1.iloc[:, 36]))
+        if self.df_mode2 is not None:
+            self.df_mode2['lower_eq'] = (4.76 / (self.df_mode2.iloc[:, 31])) * ((2 * self.df_mode2.iloc[:, 34])
+                                                                                + (0.75 * self.df_mode2.iloc[:, 36]))
+
+    def Qd(self):
+        self.df['Qd1_upper'] = None
+        self.df['Qd1_lower'] = None
+        self.df['Qd2_upper'] = None
+        self.df['Qd2_lower'] = None
+
+        self.df['Qd1_upper'] = None
+        self.df['Qd1_lower'] = None
+        self.df['Qd2_upper'] = None
+        self.df['Qd2_lower'] = None
+
+        self.df['Qd1_upper'] = None
+        self.df['Qd1_lower'] = None
+        self.df['Qd2_upper'] = None
+        self.df['Qd2_lower'] = None
+        pass
+
+
+    def Epsilon(self):
+    self.df['Epsilontr1'] = None
+    self.df['Epsilontr2'] = None
+
+    def Qs(self):
+    self.df['Qs'] = None
+
+
+    def Z(self):
+        if self.df_mode0 is not None:
+            self.df_mode0['Z'] = 1
+        if self.df_mode1 is not None:
+            self.df_mode1['Z'] = (10000 * self.df_mode1['Xtr %']) / (
+                        (10000 * self.df_mode1['Xtr %']) - (self.df_mode1[' ξtr 1,2 ppmv']))
+        if self.df_mode2 is not None:
+            if self.df_mode2['Tracer gas type']=='1':
+                self.df_mode2['Z'] = 1 + (self.df_mode2['Qd Ar/CO2 upper']*(self.df_mode2[' ξtr 1,2 ppmv'])
+                                          pass
+             elif self.df_mode2['Tracer gas type']=='2':
+                                          pass
+
+
--- src/main.py
+++ src/main.py
@@ -1,16 +1,18 @@
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 from classes import Workbook
-import basicvariables
-from pandas import ExcelFile
-from pandas import ExcelWriter
 
+import warnings
 
 def main():
+    warnings.filterwarnings("ignore")
     workbook_name = input("please input the filepath of workbook you will be working with today: ")
     test = Workbook(workbook_name, 15)
-    print(test.df_mode0.iloc[:,-2])
+    test.upper_eq()
+    test.lower_eq()
+    test.Qd()
+    test.concat()
+
+
+
 if __name__ == "__main__":
     main()

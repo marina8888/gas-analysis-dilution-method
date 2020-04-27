402580aa595013eb8970d3865987c3e25cdda9fc
Marina Kovaleva <38610356+marina8888@users.noreply.github.com>
2020-04-27 23:43:12 +0900

    added excel test file

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ def main():
     # workbook_name = input("please input the filepath of workbook you will be working with today: ")
     # test = Workbook(workbook_name, 15, gas_list)
 
-    test = Workbook('/Users/marina/Developer/GitHub/gas-analysis-dilution-method/excel/test1.xlsx', 15,
+    test = Workbook('test1.xlsx', 15,
                     ['O2', 'N2', 'Air', 'CH4', 'Blah', 'Blah2'])
     # fill columns for basic/inital values
     print(test.df.columns.values)

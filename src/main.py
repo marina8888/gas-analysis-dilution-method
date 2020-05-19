5bc6fee9d8f53aaac50675fe58d63189b0b90d22
Marina Kovaleva <38610356+marina8888@users.noreply.github.comda>
2020-05-19 20:16:47 +0900

    df filter for sol

--- src/main.py
+++ src/main.py
@@ -45,25 +45,25 @@ def main():
     #     '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/N2RESULTSbook15finaledit(04012020).xlsx',
     #     15,
     #     ['CO', 'H2O', 'NO', 'NO2', 'N2O', 'NH3'], ['H2', 'O2'])
-    # test9 = Workbook('/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/CO2RESULTSbook13final(21022020).xlsx', 15, ['CO', 'H2O', 'NO', 'NO2', 'N2O', 'NH3'], ['H2', 'O2'])
-    test10 = Workbook(
-         '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/60ERESULTSbook7final(30012020).xlsx',
-         8,
-         ['CO', 'H2O', 'NO', 'NO2', 'N2O', 'NH3'], ['H2', 'O2'])
+    test9 = Workbook('/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/CO2RESULTSbook13final(21022020).xlsx', 15, ['CO', 'H2O', 'NO', 'NO2', 'N2O', 'NH3'], ['H2', 'O2'])
+    # test10 = Workbook(
+    #      '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/60ERESULTSbook7final(30012020).xlsx',
+    #      8,
+    #      ['CO', 'H2O', 'NO', 'NO2', 'N2O', 'NH3'], ['H2', 'O2'])
     # create all columns and calculate uncertainties for all workbooks:
-    instance_list = [test1, test10]
+    instance_list = [test9]
     for instance in instance_list:
         create_workbook.create_workbook(instance)
 
     # save all data with complete uncertainty calcs to one large csv file:
     my_workbook = BigWorkbook(instance_list)
-    # my_workbook.df.to_csv('../excel_external/image_plots/my_workbook.csv')
-
-    # create lists and labels for all objects that need plotting. These can be modified to suit the plots:
-    title = 'Gas Concentration vs Dilution Method 60% Heat Ratio'
-
-    # plot graph_creator for all gases and lists given above as input parameters to the graph_creator:
-    create_graphs.plot_by_workbook(test1.full_gas_list, my_workbook, title)
+    my_workbook.df.to_csv('../excel_external/image_plots/my_workbook.csv')
+    #
+    # # create lists and labels for all objects that need plotting. These can be modified to suit the plots:
+    # title = 'Gas Concentration vs Dilution Method 60% Heat Ratio'
+    #
+    # # plot graph_creator for all gases and lists given above as input parameters to the graph_creator:
+    # create_graphs.plot_by_workbook(test1.full_gas_list, my_workbook, title)
 
 
 if __name__ == "__main__":

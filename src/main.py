d692adab3adca9e61287b27774df5b81a6b9053b
Marina Kovaleva <38610356+marina8888@users.noreply.github.com>
2020-06-09 23:45:06 +0900

    add correct plot settings

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ def main():
     warnings.filterwarnings("ignore")
     # import workbooks as dataframe objects
     test1 = Workbook(
-        '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/60ERESULTSbook16final(0404020).xlsm',
+        '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/60All.xlsx',
         15,
          ['CO', 'H2O', 'NO', 'NO2', 'N2O', 'NH3'], ['H2', 'O2'])
     test2 = Workbook(
@@ -21,21 +21,21 @@ def main():
         15,
           ['CO', 'H2O', 'NO', 'NO2', 'N2O', 'NH3'], ['H2', 'O2'])
     test3 = Workbook(
-        '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/40RESULTSbook19final(07042020)new.xlsx',
+        '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/40All.xlsx',
         21,
         ['CO', 'H2O', 'NO', 'NO2', 'N2O', 'NH3'], ['H2', 'O2'])
     test4 = Workbook(
-        '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/30RESULTSbook20final(08042020).xlsx',
+        '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/30All.xlsx',
         24,
         ['CO', 'H2O', 'NO', 'NO2', 'N2O', 'NH3'], ['H2', 'O2'])
     test5 = Workbook(
-        '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/20ERESULTSbook21final(09042020).xlsx',
+        '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/20All.xlsx',
         24,
         ['CO', 'H2O', 'NO', 'NO2', 'N2O', 'NH3'], ['H2', 'O2'])
     test6 = Workbook(
-        '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/10RESULTSBook22final(11042020).xlsx',
+        '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/10All.xlsx',
         24,
-        ['CO', 'H2O', 'NO', 'NO2', 'N2O', 'NH3'], ['H2', 'O2'])
+         ['CO', 'H2O', 'NO', 'NO2', 'N2O', 'NH3'], ['H2', 'O2'])
 
     # test7 = Workbook(
     #     '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/N2ARESULTSbook12final(20022020).xlsx',
@@ -51,20 +51,23 @@ def main():
     #      '/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/60ERESULTSbook7final(30012020).xlsx',
     #      8,
     #      ['CO', 'H2O', 'NO', 'NO2', 'N2O', 'NH3'], ['H2', 'O2'])
+    # test12 = Workbook('/Users/marina/Documents/Work/Tohoku Uni/strain stabiolised product gas/plotting_data/30RESULTSbook20(08042020).xlsx', 24,
+    #       ['CO', 'H2O', 'NO', 'NO2', 'N2O', 'NH3'], ['H2', 'O2'])
     # create all columns and calculate uncertainties for all workbooks:
     instance_list = [test1, test2, test3, test4, test5, test6]
+
     for instance in instance_list:
         create_workbook.create_workbook(instance)
 
     # save all data with complete uncertainty calcs to one large csv file:
     all_heat_workbook = BigWorkbook(instance_list)
-    all_heat_workbook.df.to_csv('../excel_external/image_plots/main.csv')
+    all_heat_workbook.df.to_csv('../excel_external/image_plots/all_results_ever.csv')
     #
     # # create lists and labels for all objects that need plotting. These can be modified to suit the plots:
     # title = 'Gas Concentration vs Dilution Method 60% Heat Ratio'
     #
-    # # plot graph_creator for all gases and lists given above as input parameters to the graph_creator:
-    # create_graphs.plot_by_workbook(test1.full_gas_list, my_workbook, title)
+    # # # plot graph_creator for all gases and lists given above as input parameters to the graph_creator:
+    # create_graphs.plot_by_workbook(test12.full_gas_list, instance_list, title, colour_list= ['r'])
 
 
 if __name__ == "__main__":

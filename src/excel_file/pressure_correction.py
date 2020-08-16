0a89a67f5385f3b01f41ebd7aec2f88fa5b96605
Marina Kovaleva <38610356+marina8888@users.noreply.github.com>
2020-08-16 13:15:42 +0900

    frame for pressure correction

--- src/excel_file/pressure_correction.py
+++ src/excel_file/pressure_correction.py
@@ -1,13 +1,18 @@
 from excel_file.classes import Workbook
+import pandas as pd
 
 
 # dataframe object input is adjusted for pressure dependency of FTIR sensor
 
-def pressure_correct(instance: Workbook, pressure_correction_file: str):
+def pressure_correct(workbook: Workbook, pressure_correction_file: str):
     print("adding pressure correction")
     if pressure_correction_file.endswith('.csv'):
-        pass
+        p_df = pd.read_excel(pressure_correction_file, sheet_name='Main')
+        workbook['name of temp mfm']
+        workbook['name of FT mfm']
     elif pressure_correction_file.endswith('xlsx'):
-        pass
+        p_df = pd.read_csv(pressure_correction_file)
+        workbook['name of temp mfm']
+        workbook['name of FT mfm']
     else:
         raise TypeError('ensure pressure correction file format must be .xlsx or .csv')
